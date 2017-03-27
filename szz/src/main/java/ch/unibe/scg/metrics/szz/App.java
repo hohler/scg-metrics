5633b34abe9ceceacff7574c2e2e5380cd8983a8
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2017-03-27 12:40:19 +0200

    szz init project

---
+++ szz/src/main/java/ch/unibe/scg/metrics/szz/App.java
@@ -0,0 +1,13 @@
+package ch.unibe.scg.metrics.szz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
