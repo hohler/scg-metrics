3b04e7f33b9fd6ed10239523b11093d9fabbd612
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2017-04-10 16:14:29 +0200

    updates

--- changemetrics/src/main/java/ch/unibe/scg/metrics/changemetrics/ChangeMetrics.java
+++ changemetrics/src/main/java/ch/unibe/scg/metrics/changemetrics/ChangeMetrics.java
@@ -3,15 +3,19 @@ package ch.unibe.scg.metrics.changemetrics;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.repodriller.RepoDriller;
 import org.repodriller.domain.ChangeSet;
+import org.repodriller.domain.Commit;
 import org.repodriller.filter.range.CommitRange;
 import org.repodriller.filter.range.Commits;
 import org.repodriller.scm.GitRemoteRepository;
 import org.repodriller.scm.GitRepository;
+import org.repodriller.scm.SCM;
 import org.repodriller.scm.SCMRepository;
 
 import ch.unibe.scg.metrics.changemetrics.domain.CMRepository;
@@ -58,6 +62,10 @@ public class ChangeMetrics {
 	
 	public void setRange(Calendar from, Calendar to) {
 		this.range = Commits.betweenDates(from, to);
+	}
+	
+	public void setRange(CommitRange range) {
+		this.range = range;
 	}
 	
 	/**
@@ -129,4 +137,50 @@ public class ChangeMetrics {
 		
 		commitList = results;
 	}
+	
+	/**
+	 * for each commit get a range that is from the commit to x weeks back
+	 * @param weeksBack
+	 */
+	public Map<String, CommitRange> generateCommitListWithWeeks(int weeksBack) {
+		Map<String, CommitRange> results = new LinkedHashMap<>();
+		SCM scm = repository.getScm();
+		List<ChangeSet> changeSets = scm.getChangeSets();
+		int counter = 0;
+		for(ChangeSet cs : changeSets) {
+			if(counter % everyNthCommit == 0 || everyNthCommit == 1) {
+				String ref = cs.getId();
+				Commit c = scm.getCommit(ref);
+				
+				if(!c.isInMainBranch()) continue; // only MAIN branch!
+				
+				Calendar end = Calendar.getInstance();
+				end.setTimeInMillis(c.getDate().getTimeInMillis());
+				end.add(Calendar.SECOND, -10); // So that this commit will be in range
+				
+				Calendar start = Calendar.getInstance();
+				start.setTimeInMillis(c.getDate().getTimeInMillis());
+				start.add(Calendar.WEEK_OF_YEAR, -weeksBack);
+				start.add(Calendar.SECOND, -10);
+
+				CommitRange range = Commits.betweenDates(start,  end);
+				
+				results.put(ref, range);
+			}
+			counter++;
+		}
+		return results;
+	}
+	
+
+	/*public Calendar calculateFirstRefWeeks(String ref, int weeksBack) {
+		Commit c = this.repository.getScm().getCommit(ref);
+		Calendar date = c.getDate();
+		
+		Calendar start = Calendar.getInstance();
+		start.setTimeInMillis(date.getTimeInMillis());
+		start.add(Calendar.WEEK_OF_YEAR, -weeksBack);
+		
+		return start;
+	}*/
 }
