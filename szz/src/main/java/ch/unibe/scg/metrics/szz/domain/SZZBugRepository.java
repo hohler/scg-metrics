06a45f2c91f227af983ebc778e6f36a0b832432d
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2017-07-06 09:38:29 +0200

    szz: prevent NPE

--- szz/src/main/java/ch/unibe/scg/metrics/szz/domain/SZZBugRepository.java
+++ szz/src/main/java/ch/unibe/scg/metrics/szz/domain/SZZBugRepository.java
@@ -24,14 +24,17 @@ public class SZZBugRepository {
 	}
 	
 	public void addBugCommit(String commitHash) {
+		if(this.bugCommits == null) return;
 		this.bugCommits.add(commitHash);
 	}
 	
 	public boolean removeBugCommit(String commitHash) {
+		if(this.bugCommits == null) return false;
 		return this.bugCommits.remove(commitHash);
 	}
 	
 	public boolean isBugfixCommit(String commitHash) {
+		if(this.bugCommits == null) return false;
 		return bugCommits.contains(commitHash);
 	}
 }
